551be255a906187cb616848520397c63ccb66f15
gpsouza <gpsouza1947@gmail.com>
2025-09-27 05:48:06 -0300

    Implementação de modelo de sumarização e melhorias no template HTML. Atualização das dependências no requirements.txt para incluir 'torch' e 'transformers'.

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, app, render_template
+from flask import Flask, render_template
+from transformers import pipeline
 
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+# Carrega um modelo de sumarização 
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+@app.route("/", methods=['POST'])
 def template():
+
+    """ 
+        # Hucgging Face Summarization
+            try:
+            generator = pipeline(
+                "text-generation", 
+                model="gpt2", 
+                device=device
+            )
+            print("Modelo 'gpt2' carregado com sucesso!")
+            except Exception as e:
+            print(f"Erro ao carregar o modelo: {e}")
+    """
+       
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "This a about page"
-
+if __name__ == '__main__':
+    # Inicia o servidor local Flask
+    app.run(debug=True)
